115b840cd796f09099df047d373db7068d5710c5
Ntres <84086409+Ntres@users.noreply.github.com>
2021-06-03 09:42:50 +0200

    first commit

---
+++ src/app/pages/characters-page/characters-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CharactersService } from 'src/app/shared/services/characters.service';
+
+@Component({
+  selector: 'app-characters-page',
+  templateUrl: './characters-page.component.html',
+  styleUrls: ['./characters-page.component.scss']
+})
+export class CharactersPageComponent implements OnInit {
+
+  characters: any;
+
+  constructor(private charactersService: CharactersService) { }
+
+  ngOnInit(): void {
+
+     this.charactersService.getCharacters().subscribe((charactersData: any) => {
+      this.characters = charactersData.results;
+    });
+  }
+
+}
